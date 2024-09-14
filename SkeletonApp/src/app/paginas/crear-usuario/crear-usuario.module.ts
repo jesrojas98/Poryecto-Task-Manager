66d463b467c23336b28c409d211b56f1ee9d2c13
jesrojas98 <jes.rojasg@duocuc.cl>
2024-09-14 20:46:48 -0300

    PRIMERA ENTREGA

---
+++ SkeletonApp/src/app/paginas/crear-usuario/crear-usuario.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { CrearUsuarioPageRoutingModule } from './crear-usuario-routing.module';
+import { addIcons } from 'ionicons';
+import { heart, logoApple, settingsSharp, star } from 'ionicons/icons';
+import { CrearUsuarioPage } from './crear-usuario.page';
+import { InputEmailComponent } from 'src/app/componentes/input-email/input-email.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    CrearUsuarioPageRoutingModule,   
+  ],
+  declarations: [CrearUsuarioPage, InputEmailComponent]
+})
+export class CrearUsuarioPageModule {}
